e21e3b12abec9514a0c587607e22a3ba72ae5ebc
pavelgreblov <greblovpn@yandex.ru>
2023-06-28 02:59:01 +0300

    style:

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.booking.exception.BookingValidationException;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.exception.ItemRequestValidationException;
 import ru.practicum.shareit.request.service.ItemRequestService;
